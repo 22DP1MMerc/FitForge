849afb38dbf815fbf5cc172ca562b604b8dec1eb
marts <mm00448@rvt.lv>
2026-05-12 09:38:51 +0300

    route konflikti

--- routes/web.php
+++ routes/web.php
@@ -80,11 +80,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 // Profils
-Route::middleware('auth')->group(function () {
-    Route::get('/profile',    [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile',  [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//    Route::get('/profile',    [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile',  [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
 
 // Treniņu žurnāls
 Route::middleware('auth')->prefix('workout-logs')->group(function () {
